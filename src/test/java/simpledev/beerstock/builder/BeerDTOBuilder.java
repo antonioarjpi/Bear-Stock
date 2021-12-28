3fd2a9172634b2e08ec8217e6259008baf868142
antonioarjpi <antonioarjpi@gmail.com>
2021-12-28 18:16:56 -0300

    Test - Service, builder

---
+++ src/test/java/simpledev/beerstock/builder/BeerDTOBuilder.java
@@ -0,0 +1,36 @@
+package simpledev.beerstock.builder;
+
+import lombok.Builder;
+import simpledev.beerstock.dto.BeerDTO;
+import simpledev.beerstock.enums.BeerType;
+
+@Builder
+public class BeerDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String name = "Brahma";
+
+    @Builder.Default
+    private String brand = "Ambev";
+
+    @Builder.Default
+    private int max = 50;
+
+    @Builder.Default
+    private int quantity = 10;
+
+    @Builder.Default
+    private BeerType type = BeerType.LAGER;
+
+    public BeerDTO toBeerDTO() {
+        return new BeerDTO(id,
+                name,
+                brand,
+                max,
+                quantity,
+                type);
+    }
+}
---
+++ src/test/java/simpledev/beerstock/service/BeerServiceTest.java
@@ -0,0 +1,48 @@
+package simpledev.beerstock.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import simpledev.beerstock.builder.BeerDTOBuilder;
+import simpledev.beerstock.dto.BeerDTO;
+import simpledev.beerstock.exception.BeerAlreadyRegisteredException;
+import simpledev.beerstock.mapper.BeerMapper;
+import simpledev.beerstock.model.Beer;
+import simpledev.beerstock.repository.BeerRepository;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class BeerServiceTest {
+
+    private static final long INVALID_BEER_ID = 1L;
+
+    @Mock
+    private BeerRepository beerRepository;
+
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @InjectMocks
+    private BeerService beerService;
+
+    @Test
+    void whenBeerInformedTheIShuldBeCreated() throws BeerAlreadyRegisteredException {
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer expectedSavedBeer = beerMapper.toModel(beerDTO);
+
+        when(beerRepository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
+        when(beerRepository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
+
+        BeerDTO createdBeerDTO = beerService.createBeer(beerDTO);
+
+        assertEquals(beerDTO.getId(), createdBeerDTO.getId());
+        assertEquals(beerDTO.getName(), createdBeerDTO.getName());
+
+    }
+}
